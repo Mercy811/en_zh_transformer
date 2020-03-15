baa3ee881e07499dac1631173ac7d2ea27f54300
Mercy811 <xinyiye811@gmail.com>
2020-03-15 12:25:38 +0800

    build GUI

--- website/app.py
+++ website/app.py
@@ -1,16 +1,49 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify, request
+from flask_wtf import FlaskForm
+from flask_pagedown import PageDown
+from flask_pagedown.fields import PageDownField
+from wtforms.fields import SubmitField
+import requests
+import json
+import re
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!' # secruely sigining the session cookie
+pagedown = PageDown(app)
 
 
-app = Flask(__name__) # create a instance of the Flask object
-
-# add a function that returns content
-# use Flask's app.route decorator to map the URL route / to that function:
-@app.route("/")
-@app.route("/hello/<name>")
-def hello_there(name):
-    return render_template(
-        "base.html", name=name #render_template assumes that the first argument is relative to the templates folder
-    )
+class PageDownFormExample(FlaskForm):
+    pagedown = PageDownField('Type the text you want to translate and click "Translate" button below.')
+    submit = SubmitField('Translate')
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = PageDownFormExample()
+    text = None
+    language = "en-fr" #Default
+    if form.validate_on_submit():
+        source = form.pagedown.data.lower()
+        source = re.sub(r"([?.!,:;¿])", r" \1 ", source)
+        source = re.sub(r'[" "]+', " ", source)
+        language = str(request.form.get('lang'))
+        if language == "en-fr":
+            url = "http://127.0.0.1:5000/translator/translate"
+        elif language == "fr-en":
+            url = "http://127.0.0.1:3000/translator/translate"
+        headers = {"Content-Type": "application/json"}
+        data = [{"src": source, "id": 100}]
+        response = requests.post(url, json=data, headers=headers)
+        translation = response.text
+        jsn = json.loads(translation)
+        text = jsn[0][0]['tgt']
+        text = re.sub(r" ([?.!,:،؛؟¿])", r"\1", text)
+    else:
+        form.pagedown.data = ('This is a very simple test.')
+    return render_template('index.html', form=form, language=language, text=text)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
