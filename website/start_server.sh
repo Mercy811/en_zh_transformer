cbb8e3744cabbc7d001be5c81e2d0c00885356fd
Mercy811 <xinyiye811@gmail.com>
2020-03-23 18:06:57 +0800

    add argument: config_file_name

--- website/start_server.sh
+++ website/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# start_server.sh path/to/OpenNMT-tf
+# /bin/bash start_server.sh path/to/OpenNMT-tf config_file_name
 # run this file to start REST server 
 
 # the following line is used to get the path when this script is inside OpenNMT-tf folder
@@ -10,8 +10,8 @@ DIR=$1
 cd $DIR
 
 export IP="0.0.0.0"
-export PORT=5000
+export PORT=8080
 export URL_ROOT="/translator"
-export CONFIG="${DIR}/available_models/flask.conf.json"
+export CONFIG="${DIR}/available_models/$2"
 
 python3 server.py --ip $IP --port $PORT --url_root $URL_ROOT --config $CONFIG
